f6df06c38c05a83fab3d9c17a3b7b6800aacf644
Joël Charles <joel.charles91@gmail.com>
2024-07-12 12:53:05 +0200

    chore(ci): do not run Deno tests on Windows

--- packages/adapter-express/tests/express.spec.ts
+++ packages/adapter-express/tests/express.spec.ts
@@ -1,6 +1,7 @@
 import { type Run, runTests } from "@universal-middleware/tests";
 import * as vitest from "vitest";
 
+const isWin = process.platform === "win32";
 let port = 3100;
 
 const runs: Run[] = [
@@ -14,12 +15,16 @@ const runs: Run[] = [
     command: "pnpm run test:run-express:bun",
     port: port++,
   },
-  {
+];
+
+if (!isWin) {
+  // Deno tests are too slow on Windows
+  runs.push({
     name: "adapter-express: deno",
     command: "pnpm run test:run-express:deno",
     port: port++,
-  },
-];
+  });
+}
 
 runTests(runs, {
   vitest,
--- packages/adapter-hono/tests/hono.spec.ts
+++ packages/adapter-hono/tests/hono.spec.ts
@@ -1,6 +1,7 @@
 import { type Run, runTests } from "@universal-middleware/tests";
 import * as vitest from "vitest";
 
+const isWin = process.platform === "win32";
 let port = 3000;
 
 const runs: Run[] = [
@@ -14,12 +15,16 @@ const runs: Run[] = [
     command: "pnpm run test:run-hono:bun",
     port: port++,
   },
-  {
+];
+
+if (!isWin) {
+  // Deno tests are too slow on Windows
+  runs.push({
     name: "adapter-hono: deno",
     command: "pnpm run test:run-hono:deno",
     port: port++,
-  },
-];
+  });
+}
 
 runTests(runs, {
   vitest,
