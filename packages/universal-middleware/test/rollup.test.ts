ce61d2b496c14475f0628a3c741fc062ad243e39
Joël Charles <joel.charles91@gmail.com>
2024-07-16 16:07:20 +0200

    tests: rollup

--- packages/universal-middleware/test/rollup.test.ts
+++ packages/universal-middleware/test/rollup.test.ts
@@ -6,52 +6,6 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import unplugin from "../src/build";
 
 describe("rollup", () => {
-  function testRollupHandlers(gen: RollupOutput) {
-    const hattip = gen.output.find(
-      (f: any) =>
-        f.facadeModuleId ===
-        "virtual:universal-middleware:hattip:handler:test/handler.ts",
-    ) as OutputChunk | undefined;
-    expect(hattip?.name).toEqual("universal-hattip-handler");
-
-    const express = gen.output.find(
-      (f: any) =>
-        f.facadeModuleId ===
-        "virtual:universal-middleware:express:handler:test/handler.ts",
-    ) as OutputChunk | undefined;
-    expect(express?.name).toEqual("universal-express-handler");
-
-    const hono = gen.output.find(
-      (f: any) =>
-        f.facadeModuleId ===
-        "virtual:universal-middleware:hono:handler:test/handler.ts",
-    ) as OutputChunk | undefined;
-    expect(hono?.name).toEqual("universal-hono-handler");
-  }
-
-  function testRollupMiddlewares(gen: RollupOutput) {
-    const hattip = gen.output.find(
-      (f: any) =>
-        f.facadeModuleId ===
-        "virtual:universal-middleware:hattip:middleware:test/middleware.ts",
-    ) as OutputChunk | undefined;
-    expect(hattip?.name).toEqual("universal-hattip-middleware");
-
-    const express = gen.output.find(
-      (f: any) =>
-        f.facadeModuleId ===
-        "virtual:universal-middleware:express:middleware:test/middleware.ts",
-    ) as OutputChunk | undefined;
-    expect(express?.name).toEqual("universal-express-middleware");
-
-    const hono = gen.output.find(
-      (f: any) =>
-        f.facadeModuleId ===
-        "virtual:universal-middleware:hono:middleware:test/middleware.ts",
-    ) as OutputChunk | undefined;
-    expect(hono?.name).toEqual("universal-hono-middleware");
-  }
-
   it("generates all server files (string input)", async () => {
     const result = await rollup({
       input: "test/handler.ts?handler",
@@ -124,3 +78,49 @@ describe("rollup", () => {
     testRollupMiddlewares(gen);
   });
 });
+
+function testRollupHandlers(gen: RollupOutput) {
+  const hattip = gen.output.find(
+    (f: any) =>
+      f.facadeModuleId ===
+      "virtual:universal-middleware:hattip:handler:test/handler.ts",
+  ) as OutputChunk | undefined;
+  expect(hattip?.name).toEqual("universal-hattip-handler");
+
+  const express = gen.output.find(
+    (f: any) =>
+      f.facadeModuleId ===
+      "virtual:universal-middleware:express:handler:test/handler.ts",
+  ) as OutputChunk | undefined;
+  expect(express?.name).toEqual("universal-express-handler");
+
+  const hono = gen.output.find(
+    (f: any) =>
+      f.facadeModuleId ===
+      "virtual:universal-middleware:hono:handler:test/handler.ts",
+  ) as OutputChunk | undefined;
+  expect(hono?.name).toEqual("universal-hono-handler");
+}
+
+function testRollupMiddlewares(gen: RollupOutput) {
+  const hattip = gen.output.find(
+    (f: any) =>
+      f.facadeModuleId ===
+      "virtual:universal-middleware:hattip:middleware:test/middleware.ts",
+  ) as OutputChunk | undefined;
+  expect(hattip?.name).toEqual("universal-hattip-middleware");
+
+  const express = gen.output.find(
+    (f: any) =>
+      f.facadeModuleId ===
+      "virtual:universal-middleware:express:middleware:test/middleware.ts",
+  ) as OutputChunk | undefined;
+  expect(express?.name).toEqual("universal-express-middleware");
+
+  const hono = gen.output.find(
+    (f: any) =>
+      f.facadeModuleId ===
+      "virtual:universal-middleware:hono:middleware:test/middleware.ts",
+  ) as OutputChunk | undefined;
+  expect(hono?.name).toEqual("universal-hono-middleware");
+}
