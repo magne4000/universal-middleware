b1bb8897a0a4bebee14336356b1bb12dd3ba9d60
Joël Charles <joel.charles91@gmail.com>
2024-08-19 08:44:09 +0200

    feat: adapter-fastify

---
+++ packages/adapter-fastify/src/index.ts
@@ -0,0 +1,7 @@
+export {
+  createHandler,
+  createMiddleware,
+  getContext,
+  type FastifyHandler,
+  type FastifyMiddleware,
+} from "./common.js";
---
+++ packages/adapter-fastify/tests/fastify.spec.ts
@@ -0,0 +1,31 @@
+import { type Run, runTests } from "@universal-middleware/tests";
+import * as vitest from "vitest";
+
+let port = 3100;
+
+const runs: Run[] = [
+  {
+    name: "adapter-fastify: node",
+    command: "pnpm run test:run-fastify:node",
+    port: port++,
+  },
+  {
+    name: "adapter-fastify: bun",
+    command: "pnpm run test:run-fastify:bun",
+    port: port++,
+  },
+  {
+    name: "adapter-fastify: deno",
+    command: "pnpm run test:run-fastify:deno",
+    port: port++,
+  },
+];
+
+runTests(runs, {
+  vitest,
+  test(response) {
+    // added by helmet
+    vitest.expect(response.headers.has("content-security-policy")).toBe(true);
+    vitest.expect(response.headers.has("x-xss-protection")).toBe(true);
+  },
+});
