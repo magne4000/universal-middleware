d217194002f86136516fde02c48f9e0935f8c9a7
Joël Charles <joel.charles91@gmail.com>
2024-08-21 09:37:36 +0200

    chore: release script

---
+++ release.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Function to execute a command in a specified directory
+execute_in_directory() {
+    local target_dir=$1
+    shift
+    local command=$@
+
+    # Save the current directory
+    local original_dir=$(pwd)
+
+    # Change to the target directory
+    cd "$target_dir" || { echo "Failed to change directory"; return 1; }
+
+    # Execute the command
+    $command
+
+    # Return to the original directory
+    cd "$original_dir"
+}
+
+
+prompt_and_release() {
+    local package=$1
+
+    # Prompt the user with multiple choices
+    echo "Please choose an option:"
+    echo "1) release"
+    echo "2) release:minor"
+    echo "3) release:commit"
+    echo "4) none"
+    read -p "Enter your choice (1/2/3/4): " user_choice
+
+    case $user_choice in
+        1)
+            echo "You chose: release"
+            execute_in_directory "packages/$package" pnpm run release
+            ;;
+        2)
+            echo "You chose: release:minor"
+            execute_in_directory "packages/$package" pnpm run release:minor
+            ;;
+        3)
+            echo "You chose: release:commit"
+            execute_in_directory "packages/$package" pnpm run release:commit
+            ;;
+        *)
+            ;;
+    esac
+}
+
+prompt_and_release "core"
+prompt_and_release "adapter-express"
+prompt_and_release "adapter-fastify"
+prompt_and_release "adapter-h3"
+prompt_and_release "adapter-hattip"
+prompt_and_release "adapter-hono"
+prompt_and_release "adapter-webroute"
+prompt_and_release "universal-middleware"
