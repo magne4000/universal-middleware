84e95352ccfea5199593db4b97c3e2e1cc71d3c5
Joël Charles <joel.charles91@gmail.com>
2024-08-29 15:05:02 +0200

    chore

--- packages/adapter-hono/functions/_middleware.ts
+++ packages/adapter-hono/functions/_middleware.ts
@@ -3,6 +3,7 @@ import { handleMiddleware } from "hono/cloudflare-pages";
 import { createMiddleware } from "../src/index.js";
 
 export const onRequest = [
+  // Prefer using createMiddleware from @universal-middleware/cloudflare directly here
   handleMiddleware(createMiddleware(middlewares[0])()),
   handleMiddleware(createMiddleware(middlewares[1])()),
   handleMiddleware(createMiddleware(middlewares[2])()),
