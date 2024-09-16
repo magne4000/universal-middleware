c4340cc16741bbca0292205a678684f7e42622d6
Joël Charles <joel.charles91@gmail.com>
2024-09-16 14:24:04 +0200

    doc: `env()` recipe

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,4 @@ export { getAdapterRuntime } from "./adapter.js";
 export { mergeHeadersInto, nodeHeadersToWeb, isBodyInit } from "./utils.js";
 export { pipe } from "./pipe.js";
 export { params } from "./route.js";
+export { env } from "./env.js";
