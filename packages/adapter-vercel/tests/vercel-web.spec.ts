8af38d6447f5f1b5b90b1a833b75ba0c5c4d75d9
Joël Charles <joel.charles91@gmail.com>
2024-11-19 16:27:48 +0100

    chore: trying beta release flow

--- packages/adapter-vercel/tests/vercel-web.spec.ts
+++ packages/adapter-vercel/tests/vercel-web.spec.ts
@@ -3,10 +3,12 @@ import * as vitest from "vitest";
 
 const port = 3850;
 
+const token = process.env.VERCEL_TOKEN ? ` --token=${process.env.VERCEL_TOKEN}` : "";
+
 const runs: Run[] = [
   {
     name: "adapter-vercel: node",
-    command: "pnpm run test:run-vercel:node",
+    command: `pnpm run test:run-vercel:node${token}`,
     port,
   },
 ];
