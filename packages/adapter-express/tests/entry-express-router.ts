2c86c61efb46ff37f1ae2148f03903303cfac30a
Joël Charles <joel.charles91@gmail.com>
2025-01-09 11:37:14 +0100

    feat: express router

---
+++ packages/adapter-express/tests/entry-express-router.ts
@@ -0,0 +1,30 @@
+import { enhance } from "@universal-middleware/core";
+import { args } from "@universal-middleware/tests";
+import { handler, middlewares, routeParamHandler } from "@universal-middleware/tests/utils";
+import express from "express";
+import helmet from "helmet";
+import { apply } from "../src/index.js";
+
+const app = express();
+
+app.use(helmet());
+
+apply(app, [
+  middlewares[0](),
+  middlewares[1](),
+  middlewares[2](),
+  enhance(routeParamHandler(), {
+    path: "/user/:name",
+    method: "GET",
+  }),
+  enhance(handler(), {
+    path: "/",
+    method: "GET",
+  }),
+]);
+
+const port = args.port ? Number.parseInt(args.port) : 3000;
+
+app.listen(port, "localhost", () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
