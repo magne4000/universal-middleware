28cd382cda886440a8fa3130d52f5c0cc39d48bb
Joël Charles <joel.charles91@gmail.com>
2025-01-15 13:58:22 +0100

    chore: hattip apply

---
+++ packages/adapter-hattip/src/router.ts
@@ -0,0 +1,34 @@
+import {
+  apply as applyCore,
+  type EnhancedMiddleware,
+  getUniversal,
+  type UniversalHandler,
+  UniversalRouter,
+  type UniversalRouterInterface,
+  universalSymbol,
+} from "@universal-middleware/core";
+import { createHandler, createMiddleware } from "./common";
+import type { Router } from "@hattip/router";
+
+export class UniversalHattipRouter extends UniversalRouter implements UniversalRouterInterface {
+  #app: Router;
+
+  constructor(app: Router) {
+    super(false);
+    this.#app = app;
+  }
+
+  use(middleware: EnhancedMiddleware) {
+    this.#app.use(createMiddleware(() => getUniversal(middleware))());
+    return this;
+  }
+
+  applyCatchAll() {
+    this.#app.use("/*", createHandler(() => this[universalSymbol] as UniversalHandler)());
+  }
+}
+
+export function apply(app: Router, middlewares: EnhancedMiddleware[]) {
+  const router = new UniversalHattipRouter(app);
+  applyCore(router, middlewares);
+}
--- packages/adapter-hattip/tests/hattip.ts
+++ packages/adapter-hattip/tests/hattip.ts
@@ -1,22 +1,59 @@
 import { cors } from "@hattip/cors";
 import { createRouter } from "@hattip/router";
-import type { Get, UniversalMiddleware } from "@universal-middleware/core";
-import { handler, middlewares, routeParamHandler } from "@universal-middleware/tests/utils";
-import { createHandler, createMiddleware } from "../src/index.js";
+import {
+  enhancedMiddlewares,
+  guarded,
+  handler,
+  middlewares,
+  routeParamHandler
+} from "@universal-middleware/tests/utils";
+import { apply, createHandler, createMiddleware } from "../src/index.js";
 
 const app = createRouter();
 
 // standard hattip middleware
 app.use(cors());
 
-for (const middleware of middlewares) {
-  app.use(createMiddleware(middleware as Get<[], UniversalMiddleware>)());
+const TEST_CASE = globalThis.process?.env?.TEST_CASE;
+
+switch (TEST_CASE) {
+  case "router": {
+    apply(app, [
+      middlewares.guard,
+      middlewares.contextSync,
+      middlewares.updateHeaders,
+      middlewares.contextAsync,
+      routeParamHandler(),
+      handler(),
+    ]);
+
+    // Test registering /guarded manually to see if `guard` middleware still applies
+    app.get("/guarded", createHandler(guarded)());
+
+    break;
+  }
+  case "router_enhanced": {
+    apply(app, [
+      routeParamHandler(),
+      guarded(),
+      handler(),
+      enhancedMiddlewares.contextSync,
+      enhancedMiddlewares.updateHeaders,
+      enhancedMiddlewares.contextAsync,
+      enhancedMiddlewares.guard,
+    ]);
+
+    break;
+  }
+  default: {
+    app.use(createMiddleware(() => middlewares.guard)());
+    app.use(createMiddleware(() => middlewares.contextSync)());
+    app.use(createMiddleware(() => middlewares.updateHeaders)());
+    app.use(createMiddleware(() => middlewares.contextAsync)());
+    app.get("/user/:name", createHandler(routeParamHandler)());
+    app.get("/guarded", createHandler(guarded)());
+    app.get("/", createHandler(handler)());
+  }
 }
-
-// route params handler
-app.get("/user/:name", createHandler(routeParamHandler)());
-
-// universal handler
-app.get("/", createHandler(handler)());
 
 export default app.buildHandler();
