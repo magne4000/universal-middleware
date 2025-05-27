ba1dbc9c3bc4cd4e9b758f29c76b6ff231ae6dfe
Joël Charles <joel.charles91@gmail.com>
2025-05-27 14:45:34 +0200

    test: fix CI

--- packages/adapter-cloudflare/tests/cloudflare.spec.ts
+++ packages/adapter-cloudflare/tests/cloudflare.spec.ts
@@ -2,6 +2,7 @@ import { type Run, runTests } from "@universal-middleware/tests";
 import * as vitest from "vitest";
 
 const port = 3600;
+const delay = process.env.CI ? 3000 : 1000;
 
 const runs: Run[] = [
   {
@@ -9,28 +10,28 @@ const runs: Run[] = [
     port: port,
     command: `pnpm run test:run-cloudflare:pages --inspector-port ${port + 10000}`,
     waitUntilType: "function",
-    delay: 1000,
+    delay,
   },
   {
     name: "adapter-cloudflare: worker",
     port: port + 1,
     command: `pnpm run test:run-cloudflare:worker --define TEST_CASE:'"simple"' --inspector-port ${port + 10000 + 1}`,
     waitUntilType: "function",
-    delay: 1000,
+    delay,
   },
   {
     name: "adapter-cloudflare: worker router",
     port: port + 2,
     command: `pnpm run test:run-cloudflare:worker --define TEST_CASE:'"router"' --inspector-port ${port + 10000 + 2}`,
     waitUntilType: "function",
-    delay: 1000,
+    delay,
   },
   {
     name: "adapter-cloudflare: worker router enhanced",
     port: port + 3,
     command: `pnpm run test:run-cloudflare:worker --define TEST_CASE:'"router_enhanced"' --inspector-port ${port + 10000 + 3}`,
     waitUntilType: "function",
-    delay: 1000,
+    delay,
   },
 ];
 
