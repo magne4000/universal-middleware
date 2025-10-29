4c99da19626bb4bab0bc03e037ca16f0cb4607e0
Joël Charles <joel.charles91@gmail.com>
2025-10-29 12:58:23 +0100

    test: fix flaky tests

--- tests-examples/tests-tool/src/hono-entry.ts
+++ tests-examples/tests-tool/src/hono-entry.ts
@@ -1,13 +1,15 @@
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { serve } from "@hono/node-server";
+import compress from "@universal-middleware/compress/hono";
+import { createHandler } from "@universal-middleware/hono";
+import { sendBigFile } from "@universal-middleware/tests/utils-node";
 import handler from "@universal-middleware-examples/tool/dummy-handler-hono";
 import contextMiddleware from "@universal-middleware-examples/tool/middlewares/context-middleware-hono";
 import headersMiddleware from "@universal-middleware-examples/tool/middlewares/headers-middleware-hono";
 import paramsHandler from "@universal-middleware-examples/tool/params-handler-hono";
-import { createHandler } from "@universal-middleware/hono";
-import compress from "@universal-middleware/compress/hono";
 import { Hono } from "hono";
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-import { sendBigFile } from "@universal-middleware/tests/utils-node";
+import { args } from "./utils";
 
 const _dirname = typeof __dirname !== "undefined" ? __dirname : dirname(fileURLToPath(import.meta.url));
 
@@ -28,5 +30,18 @@ app.get("/user/:name", paramsHandler());
 app.get("/big-file", createHandler(sendBigFile)());
 
 app.get("/", handler());
+
+if (process.env.NODE_ENV === "production") {
+  const port = args.port ? Number.parseInt(args.port, 10) : 3000;
+  serve(
+    {
+      ...app,
+      port,
+    },
+    () => {
+      console.log(`Server listening on http://localhost:${port}`);
+    },
+  );
+}
 
 export default app;
