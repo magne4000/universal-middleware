e2a6887c5a910881dc9779bdde82690ebb1ebe23
Joël Charles <joel.charles91@gmail.com>
2025-11-03 11:59:30 +0100

    test: fix sendBigFile flaky tests

--- packages/tests/src/utils-node.ts
+++ packages/tests/src/utils-node.ts
@@ -1,17 +1,19 @@
-import { Readable } from "node:stream";
 import { createReadStream } from "node:fs";
 import { dirname, join } from "node:path";
-import { enhance, type Get, type UniversalHandler } from "@universal-middleware/core";
+import { Readable } from "node:stream";
 import { fileURLToPath } from "node:url";
+import { enhance, type Get, type UniversalHandler } from "@universal-middleware/core";
 
 const _dirname = typeof __dirname !== "undefined" ? __dirname : dirname(fileURLToPath(import.meta.url));
 
 export const sendBigFile: Get<[], UniversalHandler> = () =>
   enhance(
     () => {
-      const webStream = Readable.toWeb(
-        createReadStream(join(_dirname, "..", "big-file.txt"), "utf-8"),
-      ) as unknown as ReadableStream<Uint8Array>;
+      const webStream = Readable.toWeb(createReadStream(join(_dirname, "..", "big-file.txt"), "utf-8"), {
+        // Avoids error "The argument 'size' is invalid. Received NaN"
+        // See https://github.com/nodejs/node/issues/58397#issuecomment-2896460105
+        strategy: new CountQueuingStrategy({ highWaterMark: 16 }),
+      }) as unknown as ReadableStream<Uint8Array>;
 
       return new Response(webStream, {
         headers: {
