253eeeb03b8607521bf469eb183c3c92f94063cb
Joël Charles <joel.charles91@gmail.com>
2025-11-24 20:22:41 +0100

    fix(h3): dynamically import h3

--- packages/adapter-vercel/src/node/h3.ts
+++ packages/adapter-vercel/src/node/h3.ts
@@ -1,14 +1,18 @@
 import type { App } from "@universal-middleware/h3";
-import { toNodeListener } from "h3";
+import type { NodeListener } from "h3";
 import type { VercelNodeHandlerRaw } from "../utils/common.js";
 
 /**
  * H3 app to Vercel Node request handler
  */
 export function createNodeHandler(app: App): VercelNodeHandlerRaw {
-  const handler = toNodeListener(app);
+  let handler: NodeListener | undefined;
 
   return async function h3HandlerVercelNode(message, response) {
+    if (!handler) {
+      const { toNodeListener } = await import("h3");
+      handler = toNodeListener(app);
+    }
     return handler(message, response);
   };
 }
