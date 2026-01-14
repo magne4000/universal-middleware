18ed30e2f790f02146fa5222d7cfb019c156a0a8
Joël Charles <joel.charles91@gmail.com>
2026-01-14 14:56:15 +0100

    fix: dynamically import h3 (#241)

--- packages/adapter-vercel/src/edge/h3.ts
+++ packages/adapter-vercel/src/edge/h3.ts
@@ -1,14 +1,22 @@
 import type { App } from "@universal-middleware/h3";
-import { toWebHandler } from "h3";
+import type { WebHandler } from "h3";
 import type { VercelEdgeHandlerRaw } from "../utils/common.js";
 
 /**
  * H3 app to Vercel Edge request handler
  */
 export function createEdgeHandler(app: App): VercelEdgeHandlerRaw {
-  const handler = toWebHandler(app);
+  let handler: WebHandler | undefined;
 
-  return function h3HandlerVercelEdge(request) {
+  return async function h3HandlerVercelNode(request) {
+    if (!handler) {
+      try {
+        const { toWebHandler } = await import("h3");
+        handler = toWebHandler(app);
+      } catch (e) {
+        throw new Error('Failed to load "h3" package. Please ensure it is installed.', { cause: e });
+      }
+    }
     return handler(request);
   };
 }
